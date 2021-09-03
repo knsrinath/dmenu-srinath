ba96883e46b4d6aa693a4001d743fb81f635c462
knsrinath <knsrinath2005@gmail.com>
2021-09-03 13:19:45 +0530

    update

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Jetbrains Mono:size=10:weight=extrabold:antialias=true:autohint=true"
+	"Jetbrains Mono:size=9:weight=bold:antialias=true:autohint=true"
 	"FontAwesome:size=10:weight=Bold:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
@@ -18,7 +18,7 @@ static const char *colors[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 20;
+static unsigned int lineheight = 25;
 static unsigned int min_lineheight = 8;
 
 /*
